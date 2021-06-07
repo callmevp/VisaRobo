786f39b1b20ba12eb9994934f17e2b45ad93401d
Mdnoor786 <75603153+Mdnoor786@users.noreply.github.com>
2021-06-07 16:00:05 +0530

    Update misc.py

--- YoneRobot/modules/helper_funcs/misc.py
+++ YoneRobot/modules/helper_funcs/misc.py
@@ -1,7 +1,7 @@
 from math import ceil
 from typing import Dict, List
 
-from VisaRoboimport NO_LOAD
+from YoneRobotimport NO_LOAD
 from telegram import MAX_MESSAGE_LENGTH, Bot, InlineKeyboardButton, ParseMode
 from telegram.error import TelegramError
 
