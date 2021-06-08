6127d92f74c9020434eafe8e56706dc751efa405
CALLMEVP <82378057+callmevp@users.noreply.github.com>
2021-06-08 15:43:19 +0530

    Update __telegraph.py

--- VisaRobo/modules/__telegraph.py
+++ VisaRobo/modules/__telegraph.py
@@ -6,9 +6,9 @@ import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-kittu = "YONE"
+callmevp = "VISA"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=kittu)
+r = telegraph.create_account(short_name=callmevp)
 auth_url = r["auth_url"]
 
 
